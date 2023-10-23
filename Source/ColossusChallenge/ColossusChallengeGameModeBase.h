0de5d98d6a231c34482c517a5a6c3a9a1da223b4
zhtsu <zhtsu@qq.com>
2023-10-23 17:09:28 +0800

    First commit

---
+++ Source/ColossusChallenge/ColossusChallengeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ColossusChallengeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLOSSUSCHALLENGE_API AColossusChallengeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
